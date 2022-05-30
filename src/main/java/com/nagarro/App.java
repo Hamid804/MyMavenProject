d4456b04d6263e28752079b99164c2e151555283
Hamid Hussain <18bcs026@gbu.ac.in>
2022-05-30 23:09:07 +0530

    Initial commit

---
+++ src/main/java/com/nagarro/App.java
@@ -0,0 +1,13 @@
+package com.nagarro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Hamid this is jenkins practice" );
+    }
+}
