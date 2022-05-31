5020b054e40e8742dc8807bc58a489a48d7c8e7e
Hamid Hussain <80596589+Hamid804@users.noreply.github.com>
2022-05-31 09:09:13 +0530

    Update App.java

--- src/main/java/com/nagarro/App.java
+++ src/main/java/com/nagarro/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Hamid this is jenkins practice" );
+        System.out.println( "Hello Hamid this is jenkins practice committed" );
     }
 }
